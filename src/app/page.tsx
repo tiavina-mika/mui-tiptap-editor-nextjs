8e6cb8cc8194025a70363b6a87f634719ad908f9
tiavina-mika <tiavinamika@gmail.com>
2024-11-12 10:28:43 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
 import styles from "./page.module.css";
-import ThemeProvider from "./components/ThemeProvider";
-import MuiTiptapEditor from "./components/MuiTiptapEditor";
-import Footer from "./components/Footer";
-import { Metadata } from "next";
-
-export const meta: Metadata = {
+import ThemeProvider from "../components/ThemeProvider";
+import MuiTiptapEditor from "../components/MuiTiptapEditor";
+import Footer from "../components/Footer";
+import type { Metadata } from 'next'
+ 
+export const metadata: Metadata = {
   title: "mui-tiptap-editor with Next.js 14/15",
   description: "A customizable and easy to use Tiptap styled WYSIWYG rich text editor, using Material UI.",
-};
+}
 
 const HomePage = () => {
   return (
